c40ba7ca611aec8eaac3e91fcdd25b999db56669
Eze <emereuwaonueze@gmail.com>
2020-08-19 16:52:36 +0100

    fix merge conflicts

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  '/src/*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write', 'prettier --check'],
+  'src/*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write', 'prettier --check'],
   '*.{md,mdx,css,yaml,yml}': ['prettier --write', 'prettier --check'],
 };
