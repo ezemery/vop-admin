88b8d9c76636ee43a689e3d1cea963a7e198d64d
Mike <mike@tokshop.com>
2020-08-19 10:21:48 +1000

    Nest routing

---
+++ src/Components/User/User.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+    Switch,
+    Route,
+    useRouteMatch, Redirect,
+} from "react-router-dom";
+import UserId from "./UserId";
+import {UserStore} from "../../Context/store";
+
+const User = () => {
+
+    let { path } = useRouteMatch();
+
+    const {users} = React.useContext(UserStore);
+
+    return users.length === 0 ? <Redirect to="/login" /> : (
+        <Switch>
+            <Route exact path={path}>
+                { users.length > 1 ? <>Select a User</> : <Redirect to={`${path}/id/`+users[0].id} /> }
+            </Route>
+            <Route path={`${path}/id/:userId`}>
+                <UserId />
+            </Route>
+        </Switch>
+    );
+};
+
+export default User;
