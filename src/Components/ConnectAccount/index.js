48ea106e59ee913277596362734d5a118ffe23a3
Eze <emereuwaonueze@gmail.com>
2020-08-29 08:39:52 +0100

    add social setup pages

--- src/Components/ConnectAccount/index.js
+++ src/Components/ConnectAccount/index.js
@@ -1,1 +1,2 @@
-export * from './ConnectAccount'
+export * from './ConnectAccount';
+export * from './TiktokConnect';
--- src/Components/User/AccountId.js
+++ src/Components/User/AccountId.js
@@ -1,50 +1,49 @@
 import React from 'react';
-import {
-    Switch,
-    Route,
-    useRouteMatch, useParams,
-} from "react-router-dom";
-import {TikTokList} from "../TikTok";
-import {SetupScreen} from "../Setup";
-import {Embed} from "../Embed";
-import {Settings} from "../Settings";
-import {ConnectAccount} from "../ConnectAccount"
-import {AppFrame} from "../Frame";
-import {UserStore} from "../../Context/store";
-import {findUserInUsersById} from "../../services";
+import {Switch, Route, useRouteMatch, useParams} from 'react-router-dom';
+import {TikTokList} from '../TikTok';
+import {Embed} from '../Embed';
+import {Settings} from '../Settings';
+import {ConnectAccount, TiktokConnect} from '../ConnectAccount';
+
+import {AppFrame} from '../Frame';
+import {UserStore} from '../../Context/store';
+import {findUserInUsersById} from '../../services';
 
 export const AccountId = () => {
+  const {path} = useRouteMatch();
 
-    let { path } = useRouteMatch();
+  const {userId} = useParams();
+  const {users} = React.useContext(UserStore);
+  const user = findUserInUsersById(users, userId);
 
-    const { userId } = useParams();
-    const {users} = React.useContext(UserStore);
-    const user = findUserInUsersById(users, userId)
-
-    return (
-        <AppFrame>
-            <Switch>
-                <Route exact path={path}>
-
-                    <TikTokList defaultStatus="new" key="index" hideSearch={true} approvalScreen={true} user={user}/>
-                </Route>
-                <Route path={`${path}/setup`}>
-                    <SetupScreen/>
-                </Route>
-                <Route path={`${path}/manage`}>
-                    <TikTokList defaultStatus="approve" key="manage" user={user} />
-                </Route>
-                <Route path={`${path}/settings`}>
-                    <Settings/>
-
-                </Route>
-                <Route path={`${path}/embed`}>
-                    <Embed/>
-                </Route>
-                <Route path={`${path}/connect`}>
-                    <ConnectAccount/>
-                </Route>
-            </Switch>
-        </AppFrame>
-    );
+  return (
+    <AppFrame>
+      <Switch>
+        <Route exact path={path}>
+          <TikTokList
+            defaultStatus="new"
+            key="index"
+            hideSearch
+            approvalScreen
+            user={user}
+          />
+        </Route>
+        <Route path={`${path}/connect/tiktok`}>
+          <TiktokConnect />
+        </Route>
+        <Route path={`${path}/manage`}>
+          <TikTokList defaultStatus="approve" key="manage" user={user} />
+        </Route>
+        <Route path={`${path}/settings`}>
+          <Settings />
+        </Route>
+        <Route path={`${path}/embed`}>
+          <Embed />
+        </Route>
+        <Route path={`${path}/connect`}>
+          <ConnectAccount />
+        </Route>
+      </Switch>
+    </AppFrame>
+  );
 };
