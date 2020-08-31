d05d52a6a4e2bf016b6b4346307e135efcf701f7
Mike <mike@tokshop.com>
2020-08-31 13:45:49 +1000

    Fix navigation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   useHistory,
   useLocation,
   Redirect,
+  Link as ReactRouterLink,
 } from 'react-router-dom';
 import enTranslations from '@shopify/polaris/locales/en.json';
 import {AppProvider, Loading, Frame} from '@shopify/polaris';
@@ -78,8 +79,31 @@ const App = () => {
     },
   };
 
+  const IS_EXTERNAL_LINK_REGEX = /^(?:[a-z][a-z\d+.-]*:|\/\/)/;
+
+  function Link({children, url = '', external, ref, ...rest}) {
+    // react-router only supports links to pages it can handle itself. It does not
+    // support arbirary links, so anything that is not a path-based link should
+    // use a reglar old `a` tag
+    if (external || IS_EXTERNAL_LINK_REGEX.test(url)) {
+      rest.target = '_blank';
+      rest.rel = 'noopener noreferrer';
+      return (
+          <a href={url} {...rest}>
+            {children}
+          </a>
+      );
+    }
+
+    return (
+        <ReactRouterLink to={url} {...rest}>
+          {children}
+        </ReactRouterLink>
+    );
+  }
+
   return u.loading ? (
-    <AppProvider theme={theme} i18n={enTranslations}>
+    <AppProvider theme={theme} i18n={enTranslations} linkComponent={Link}>
       <Frame>
         <Loading />
       </Frame>
@@ -87,7 +111,7 @@ const App = () => {
   ) : (
     <UserStore.Provider value={{...u, fetchUserDataAsync}}>
       <VideoStore.Provider value={{...v, fetchVideoDataAsync}}>
-        <AppProvider theme={theme} i18n={enTranslations}>
+        <AppProvider theme={theme} i18n={enTranslations} linkComponent={Link}>
           <div
             className="App"
             style={{
